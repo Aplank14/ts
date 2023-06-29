d7afeb62cab5455648be3ec66268fcca489d67bc
Andy Plank <andyplank@microsoft.com>
2023-06-28 18:06:35 -0700

    updated

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,9 +1,9 @@
 export default async function Page() {
-  const res:any = await fetch("http://localhost:3000/api/isAlive");
-  if (!res.ok) {
+  // const res:any = await fetch("http://localhost:3000/api/isAlive");
+  // if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
-  const json = await res.json();
-  return <h1>Hello account holder! {json.message}</h1>
+    // throw new Error('Failed to fetch data')
+  // }
+  // const json = await res.json();
+  return <h1>Hello account holder!</h1>
 }
--- src/app/band/[id]/page.tsx
+++ src/app/band/[id]/page.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link'
+
 export async function generateStaticParams() {
   const posts = [{id:"oscos"}]; 
   return posts;
 }
 
 export default function Page({ params }: { params: { id: string } }) {
-  return <div>My Post: {params.id}</div>
+  const checkout = `/band/${params.id}/book`
+  return <>
+    <div>My Post: {params.id}</div>
+    <Link href={checkout}>Dashboard</Link>
+  </>
 }
